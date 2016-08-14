1bd0b9cbf841e2c8d7aeb3cc8bcdf50bde19bc4a
rdinter <robert.dinterman@gmail.com>
2016-08-14 14:45:32 -0400

    Paper 1 rough draft

---
+++ paper-1.R
@@ -0,0 +1,8 @@
+# ---- Start --------------------------------------------------------------
+
+net_migration <- readRDS("1-Organization/Migration/netmigration.rds")
+noaa_event    <- readRDS("0-Data/NOAA/events.rds")
+
+# ---- Manipulation -------------------------------------------------------
+
+
