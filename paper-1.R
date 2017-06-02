636ad42db292cc4451eaed4b9630741f90d0d789
Robert Dinterman <robert.dinterman@gmail.com>
2017-06-02 00:13:18 -0400

    clean up

--- paper-1.R
+++
@@ -1,8 +0,0 @@
-# ---- Start --------------------------------------------------------------
-
-net_migration <- readRDS("1-Organization/Migration/netmigration.rds")
-noaa_event    <- readRDS("0-Data/NOAA/events.rds")
-
-# ---- Manipulation -------------------------------------------------------
-
-
