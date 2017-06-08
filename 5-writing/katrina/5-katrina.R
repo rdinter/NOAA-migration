904f4d85d8c3b9fb7738972001a474a7c87e6b0c
jonathaneyer <jeyer@usc.edu>
2017-06-07 21:26:53 -0700

    update text, add 2005 regs

--- 5-writing/katrina/5-katrina.R
+++ 5-writing/katrina/5-katrina.R
@@ -23,18 +23,18 @@ star_pval <- function(p.value) {
 k_data <- read_rds("1-tidy/migration/katrina.rds") %>% 
   mutate(moved = 1*!is.na(exmpt_katrina),
          katrina = 1*katrina,
-         un_rate = unemp / (unemp + emp),
+         un_rate = 100*(unemp / (unemp + emp)),
          population = exmpt_own / 1000000,
          distance = distance / 100,
          pay = pay / 1000,
          fmr = fmr / 100,
          exmpt_katrina_eyer = exmpt_katrina_eyer*100) %>% 
-  filter(year > 1999, year < 2011) %>% 
+  filter(year > 1999, year < 2011, katrinafips == F) %>% 
   replace_na(list(return_katrina = 0, exmpt_katrina = 0, agi_katrina = 0,
                   return_katrina_eyer = 0, exmpt_katrina_eyer = 0,
                   agi_katrina_eyer = 0))
 
-# Get rid of fips which don't have full set of obs
+# Get rid of fips which don't have full set of obs and afflicted counties
 k_data <- k_data %>% 
   group_by(fips) %>% 
   filter(!any(is.na(exmpt_own) | is.na(distance) | is.na(un_rate) |
@@ -126,7 +126,8 @@ m4 <- coeftest(lp_0, vcov = cluster.vcov(lp_0, cluster = ~fips)) %>%
 
 all_models <- bind_rows(m1, m2, m3, m4)
 
-term_order <- c(m1$term, "") # for ordering variables
+varnames <- c("Intercept","Population (Millions)","Distance (Hundreds of Miles)","Unemployment Rate","Annual Pay (Thousands of USD)","Median Monthly Rent", "Non-Metro","Is 2005")
+term_order <- c(m1$term, "",varnames) # for ordering variables
 
 ols_table <- all_models %>%
   mutate(est = paste0(round(estimate, 4), star_pval(p.value)),
@@ -139,6 +140,8 @@ ols_table <- all_models %>%
 
 # Remove every other term
 ols_table[seq(2, nrow(ols_table), 2), "term"] <- ""
+# Rename variables for presentation
+ols_table[seq(1,nrow(ols_table),2), "term"] <- varnames
 
 ols_table %>% 
   select(-key) %>% 
@@ -147,4 +150,55 @@ ols_table %>%
                        IHS = as.character(round(summary(ihs_0)$adj.r.squared, 3)), 
                        LP = as.character(round(summary(lp_0)$adj.r.squared, 3)), 
                        Share = as.character(round(summary(pct_0)$adj.r.squared, 3)))) %>% 
-  knitr::kable()
+  bind_rows(data.frame(term = "Observations",
+                       Flow = as.character(nobs(reg_0)), 
+                       IHS = as.character(nobs(ihs_0)), 
+                       LP = as.character(nobs(lp_0)), 
+                       Share = as.character(nobs(pct_0)))) %>%
+  knitr::kable(caption = "\\label{tab:reg_main}Effect of Destination County Characteristics on New Orleans Outflow")
+
+# ---- reg2 ---------------------------------------------------------------
+
+m1 <- coeftest(reg_05, vcov = cluster.vcov(reg_05, cluster = ~fips)) %>% 
+  tidy() %>% mutate(model = "Flow")
+m2 <- coeftest(ihs_05, vcov = cluster.vcov(ihs_05, cluster = ~fips)) %>% 
+  tidy() %>% mutate(model = "IHS")
+m3 <- coeftest(pct_05, vcov = cluster.vcov(pct_05, cluster = ~fips)) %>% 
+  tidy() %>% mutate(model = "Share")
+m4 <- coeftest(lp_05, vcov = cluster.vcov(lp_05, cluster = ~fips)) %>% 
+  tidy() %>% mutate(model = "LP")
+
+all_models <- bind_rows(m1, m2, m3, m4)
+
+varnames <- c("Intercept","Population (Millions)","Distance (Hundreds of Miles)","Unemployment Rate","Annual Pay (Thousands of USD)","Median Monthly Rent","Non-Metro","Is 2005","Population x 2005","Distance x 2005","Unemployment Rate x 2005","Pay x 2005","Monthly Rent x 2005")
+term_order <- c(m1$term, "",varnames) # for ordering variables
+
+ols_table <- all_models %>%
+  mutate(est = paste0(round(estimate, 4), star_pval(p.value)),
+         se = paste0("(", round(std.error, 4), ")")) %>% 
+  select(model, term, est, se) %>%
+  gather(key, value, est:se) %>%
+  spread(model, value) %>% 
+  mutate(term = factor(term, levels =  term_order)) %>% 
+  arrange(term) # last one orders by the factor order, ignoring S.E.
+  
+  
+# Remove every other term
+ols_table[seq(2, nrow(ols_table), 2), "term"] <- ""
+# Rename variables for presentation
+ols_table[seq(1,nrow(ols_table),2), "term"] <- varnames
+
+ols_table %>% 
+  select(-key) %>% 
+  bind_rows(data.frame(term = "Adjusted R-Squared",
+                       Flow = as.character(round(summary(reg_05)$adj.r.squared, 3)), 
+                       IHS = as.character(round(summary(ihs_05)$adj.r.squared, 3)), 
+                       LP = as.character(round(summary(lp_05)$adj.r.squared, 3)), 
+                       Share = as.character(round(summary(pct_05)$adj.r.squared, 3)))) %>% 
+  bind_rows(data.frame(term = "Observations",
+                       Flow = as.character(nobs(reg_05)), 
+                       IHS = as.character(nobs(ihs_05)), 
+                       LP = as.character(nobs(lp_05)), 
+                       Share = as.character(nobs(pct_05)))) %>%
+  knitr::kable(caption = "\\label{tab:reg2005}Effect of Destination County Characteristics on New Orleans Outflow - 2005 Interactions")
+
